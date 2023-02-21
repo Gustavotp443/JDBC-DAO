2e1bfe7795a8f2fb9abb3624c0fb657dc7414a2c
Gustavotp443 <gustavotp443@gmail.com>
2023-02-21 00:21:22 -0300

    FindByDepartment and implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -47,19 +50,10 @@ public class SellerDaoJDBC implements SellerDao {
 					+"ON seller.DepartmentId = department.id "
 					+"WHERE seller.Id = ?");
 			st.setInt(1, id);
-			rs = st.executeQuery();	//executei a consulta
-			if(rs.next()) {	//se venho algum resultado, enquanto ocorre linha vai instanciar
-				Department dep = new Department();
-				
-				dep.setId(rs.getInt("DepartmentId"));
-				dep.setName(rs.getString("DepName"));
-				Seller obj = new Seller();
-				obj.setId(rs.getInt("Id"));
-				obj.setName(rs.getString("Email"));
-				obj.setEmail(rs.getString("Email"));
-				obj.setBaseSalary(rs.getDouble("BaseSalary"));
-				obj.setBirthDate(rs.getDate("BirthDate"));
-				obj.setDepartment(dep);
+			rs = st.executeQuery();		//executei a consulta
+			if(rs.next()) {				//se venho algum resultado, enquanto ocorre linha vai instanciar
+				Department dep = instantiateDepartment(rs);
+				Seller obj = instantiateSeller(rs, dep);
 				return obj;
 			}
 			return null;
@@ -72,9 +66,80 @@ public class SellerDaoJDBC implements SellerDao {
 		
 	}
 
+	
+
 	@Override
 	public List<Seller> findAll() {
 		return null;
 	}
+	
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs =null;
+		
+		try {
+			st= conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+"FROM seller INNER JOIN department "
+					+"ON seller.DepartmentId = department.id "
+					+"WHERE department.Id = ? "
+					+"ORDER BY Name");
+			
+			st.setInt(1, department.getId());
+			
+			rs = st.executeQuery();		//executei a consulta
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+			
+			while(rs.next()) {				//Deve ser um while pq são varios valores
+				
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				if(dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				/*O MAP É PARA EVITAR INSTANTICAR 2 DEPARTAMENTOS E SIM FAZER OS
+				SELLERS APONTAR PARA UM ÚNICO*/
+				
+				Seller obj = instantiateSeller(rs, dep);
+				list.add(obj);
+			}
+			return list;
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName(rs.getString("DepName"));
+		return dep;
+	}
+	
+	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+		 Seller obj = new Seller();
+			obj.setId(rs.getInt("Id"));
+			obj.setName(rs.getString("Email"));
+			obj.setEmail(rs.getString("Email"));
+			obj.setBaseSalary(rs.getDouble("BaseSalary"));
+			obj.setBirthDate(rs.getDate("BirthDate"));
+			obj.setDepartment(dep);
+			return obj;
+	}
+
 
 }
